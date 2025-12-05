d688eddb5c9fe62e61c5eb458d138532e2c896c5
mdsamiulislam54 <samiulm5332@gmail.com>
2025-12-05 16:35:17 +0600

    users, vehicles, booking table create

---
+++ src/config/db.ts
@@ -0,0 +1,47 @@
+
+import pg from 'pg';
+import config from '.';
+
+
+export const pool = new pg.Pool({
+    connectionString: config.connectionString,
+    ssl: false
+});
+
+export const initDB = async () => {
+ 
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(200) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL,
+            phone VARCHAR(100) NOT NULL,
+            role VARCHAR(100) NOT NULL
+        )
+    `);
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles (
+            id SERIAL PRIMARY KEY,
+            vehicle_name VARCHAR(100) NOT NULL,
+            type ENUM ('car', 'bike', 'van', 'SUV') NOT NULL,
+            registration_number VARCHAR(100) NOT NULL,
+            daily_rent_price INT NOT NULL,
+            availability_status  ENUM ('available', 'booked') NOT NULL
+        )
+    `);
+
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS bookings(
+                id  SERIAL  PRIMARY KEY ,
+                customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+                vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE ,
+                rent_start_date DATE NOT NULL,
+                rent_end_date  DATE NOT NULL,
+                total_price INT NOT NULL, 
+                status ENUM ('active', 'cancelled','returned') NOT NULL
+            )
+        `)
+};
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from 'express'
 import config from './config';
+import { initDB } from './config/db';
 
 const app = express();
+
+initDB().then(() => {
+    console.log(`Database Connection Successfully!`)
+}).catch(err => {
+    console.error('Error initializing database:', err)
+});
+
 app.use(express.json());
+
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send({
@@ -13,4 +23,4 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(config.Port, () => {
     console.log(`Server is Running Port: ${config.Port}`)
-})
+});
